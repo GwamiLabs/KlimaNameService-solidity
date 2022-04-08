9f1d60ef96717dba642b00cdd8b8179f462c5812
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-04-08 12:36:02 +0200

    bps instead of percents, retirement message can be changed, USDC&KLIMA addresses should be fixed

--- contracts/KNS_Retirer_v1.sol
+++ contracts/KNS_Retirer_v1.sol
@@ -21,6 +21,9 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
 
     // EVENTS
     event maticReceived(address indexed _sender, uint value );
+
+    // CONSTANTS
+    uint public constant MAX_BPS = 10_000; // 10000 bps = 100%
     
     // STATE VARIABLES
     address public Aggregator;
@@ -34,10 +37,11 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
     address public arKLIMA;
     address public treasurer;
     address public staking;
-    uint public slippageFactor; // 50 = 5%
-    uint public retireBCTPercent; // 50 = 5%
-    uint public stakeInKIPercent; // 50 = 5%
-    uint public percentFundsReceived; // 700 = 70%
+    string public retirementMessage = "Climate Action can become the ONLY option and it starts with YOU. KNS Domains helps you go green, stay green and socially represent green. Visit www.kns.earth for more info.";
+    uint public slippageFactor; // 500 bps = 5%
+    uint public retireBctBps; // 500 bps = 5%
+    uint public stakeInKIbps; // 500 bps = 5%
+    uint public bpsFundsReceived; // 7000 bps = 70%
     uint public USDCFromSwap;
     uint public KLIMAFromSwap;
     uint public amtLastStaked;
@@ -46,28 +50,28 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
 
     // INITIALIZER
 
-    /* function initialize(
-            address _arKLIMA,
-            address _treasurer
-        ) public initializer {
-            __Context_init_unchained();
-            __Ownable_init_unchained();
-            Aggregator = 0xEde3bd57a04960E6469B70B4863cE1c9d9363Cb8;
-            wMaticUSDCRouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
-            USDCKLIMARouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
-            KLIMAUSDC = 0x5786b267d35F9D011c4750e0B0bA584E1fDbeAD1;
-            USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-            BCT = 0x2F800Db0fdb5223b3C3f354886d907A671414A7F;
-            KLIMA = 0x4e78011Ce80ee02d2c3e649Fb657E45898257815;
-            sKLIMA = 0xb0C22d8D350C67420f06F48936654f567C73E8C8;
-            arKLIMA = _arKLIMA;
-            treasurer = _treasurer;
-            staking = 0x25d28a24Ceb6F81015bB0b2007D795ACAc411b4d;
-            slippageFactor = 50;//5%
-            retireBCTPercent = 50;//5%
-            stakeInKIPercent = 50;//5%
-            percentFundsReceived = 700;//70%
-        } */
+    function initialize(
+        address _arKLIMA,
+        address _treasurer
+    ) public initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+        Aggregator = 0xEde3bd57a04960E6469B70B4863cE1c9d9363Cb8;
+        wMaticUSDCRouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+        USDCKLIMARouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+        KLIMAUSDC = 0x5786b267d35F9D011c4750e0B0bA584E1fDbeAD1;
+        USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+        BCT = 0x2F800Db0fdb5223b3C3f354886d907A671414A7F;
+        KLIMA = 0x4e78011Ce80ee02d2c3e649Fb657E45898257815;
+        sKLIMA = 0xb0C22d8D350C67420f06F48936654f567C73E8C8;
+        arKLIMA = _arKLIMA;
+        treasurer = _treasurer;
+        staking = 0x25d28a24Ceb6F81015bB0b2007D795ACAc411b4d;
+        slippageFactor = 500; // 500 bps = 5%
+        retireBctBps = 500; // 500 bps = 5%
+        stakeInKIbps = 500; // 500 bps = 5%
+        bpsFundsReceived = 7000; // 7000 bps = 70%
+    }
 
     // OWNER
 
@@ -87,6 +91,8 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
         KLIMAUSDC = _pair;
     }
     
+    /* Fixed token addresses should not be able to be changed
+
     function set_USDC (address _token) external onlyOwner {
         USDC = _token;
     }
@@ -98,6 +104,7 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
     function set_KLIMA (address _token) external onlyOwner {
         KLIMA = _token;
     }
+    */
     
     function set_sKLIMA (address _token) external onlyOwner {
         sKLIMA = _token;
@@ -115,32 +122,41 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
         staking = _contract;
     }
     
+    function setRetirementMessage(string memory _message) external onlyOwner {
+        retirementMessage = _message;
+    }
+    
     function set_slippageFactor (uint _slippage) external onlyOwner {
         slippageFactor = _slippage;
     }
     
-    function set_retireBCTPercent (uint _percent) external onlyOwner {
-        retireBCTPercent = _percent;
+    ///@notice bps means basis points (1% = 100 bps)
+    function set_retireBctBps (uint _bps) external onlyOwner {
+        retireBctBps = _bps;
     }
     
-    function set_stakeInKIPercent (uint _percent) external onlyOwner {
-        stakeInKIPercent = _percent;
+    ///@notice bps means basis points (1% = 100 bps)
+    function set_stakeInKIbps (uint _bps) external onlyOwner {
+        stakeInKIbps = _bps;
     }
     
-    function set_percentFundsReceived (uint _percent) external onlyOwner {
-        percentFundsReceived = _percent;
+    ///@notice bps means basis points (1% = 100 bps)
+    function set_bpsFundsReceived (uint _bps) external onlyOwner {
+        bpsFundsReceived = _bps;
     }
 
     // Owner has the ability to transfer out tokens except for arKLIMA.
     // This is a function to enable recovery from errors or
     // unexpected circumstances only.
     function emergency_withdraw_other(address _token, uint _amount) external onlyOwner {
-        require(( _token != arKLIMA ), "arKLIMA cannot be withdrawn.");
+        require(_token != arKLIMA, "arKLIMA cannot be withdrawn.");
+
         uint tokenBalance = IERC20(_token).balanceOf(address(this));
-        if ( tokenBalance < _amount){
+
+        if (tokenBalance < _amount){
             _amount = tokenBalance;
         }
-        IERC20(_token).approve(msg.sender, _amount);
+
         IERC20(_token).transfer(msg.sender, _amount);
     }
     
@@ -152,19 +168,19 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
         string memory domainName
     ) public {
         require(
-            ( _USDCAmt >= 1*(10**6) ), 
+            _USDCAmt >= 1*(10**6), 
             "Payment net of royalties and referrals is less than 1 USDC."
         );
 
         IERC20(USDC).transferFrom(msg.sender, address(this), _USDCAmt);
 
         retireBCT(
-            (_USDCAmt * retireBCTPercent) / percentFundsReceived,
+            (_USDCAmt * retireBctBps) / bpsFundsReceived,
             beneficiary,
             domainName
         );
         
-        stakeinKI ((_USDCAmt * stakeInKIPercent) / percentFundsReceived );
+        stakeinKI((_USDCAmt * stakeInKIbps) / bpsFundsReceived);
         
         // transfer the remaining USDC to the treasury address
         IERC20(USDC).transfer(treasurer, IERC20(USDC).balanceOf(address(this)));
@@ -187,9 +203,8 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
             false,
             beneficiary,
             domainName,
-            "I helped the environment by minting a .klima domain. Go green and stay green!"
+            retirementMessage
         );
-
     }
 
     // Swaps USDC to Klima, stakes this, wraps this in auto-retirement KLIMA
@@ -200,8 +215,7 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
         amtLastStaked = _stakeKLIMA();
         amtLastWrapped = _wrapArKLIMA();
         arKLIMABalance += amtLastWrapped;
-    }
-        
+    } 
 
     function _swapUSDCToKlima(uint _USDCAmt) private returns (uint KlimaAmt) {
         IERC20(USDC).approve(USDCKLIMARouter, _USDCAmt);
@@ -218,13 +232,11 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
                     path[0] = token1;
         }
 
-        uint256[] memory minOut 
-                    = UKRouter.getAmountsOut(_USDCAmt, path);
+        uint256[] memory minOut = UKRouter.getAmountsOut(_USDCAmt, path);
         
-        uint[] memory  amounts = UKRouter.swapExactTokensForTokens(
+        uint[] memory amounts = UKRouter.swapExactTokensForTokens(
                                     _USDCAmt,
-                                    (minOut[1]
-                                        *(1000-slippageFactor))/1000,
+                                    (minOut[1]*(MAX_BPS-slippageFactor))/MAX_BPS,
                                     path,
                                     address(this),
                                     block.timestamp 
@@ -241,7 +253,6 @@ contract KNS_Retirer is Initializable, OwnableUpgradeable {
         IStaking(staking).claim(address(this));
         return amountToStake;
     }
-    
 
     function _wrapArKLIMA() private returns (uint) {
         uint amountToWrap = IERC20(sKLIMA).balanceOf(address(this));
